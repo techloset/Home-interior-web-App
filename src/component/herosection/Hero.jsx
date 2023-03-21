028ba9c004290814783b7a031b2be91b4b9b03b9
Tayyab Ali <decent_tayyab@gmail.com>
2023-03-21 11:07:00 +0500

    Navbar margin fix for all sides

--- src/component/herosection/Hero.jsx
+++ src/component/herosection/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
   return (
     <>
 
-      <div className=" flex w-[100%]  py-11 md:flex-row flex-col items-center relative">
+      <div className=" flex w-[100%]  mt-[68px] md:flex-row flex-col items-center relative">
   
-        <img className="object-cover object-center rounded lg:w-[60%] lg:h-[50%]" alt="hero" src={homeinterior} />
+        <img className="object-cover object-center rounded lg:w-[57%]" alt="hero" src={homeinterior} />
        
-        <div className="lg:w-[45%] lg:h-[55%] mx-auto max-md:mt-[22%] max-sm:mt-[20%] max-lg:mt-[35%]  lg:gap-[25px]  left-[5%] w-[90%]  h-[52%] flex justify-center absolute lg:left-[52%] bg-white bg-opacity-80    flex-col  items-center text-center">
+        <div className="lg:w-[51%]  mx-auto max-md:mt-[22%] max-sm:mt-[20%] max-lg:mt-[35%]  lg:gap-[25px]  left-[5%] w-[90%]  h-[52%] flex justify-center absolute lg:left-[52%] bg-white bg-opacity-80    flex-col  items-center text-center">
           <h1 className="text-sm sm:text-xl lg:mb-4 lg:pl-[3%] font-normal ">Get personalised home interiors
             <p className='text-center font-semibold text-[#241330] text-lg sm:text-3xl'>  in just 50 days </p>
           </h1>
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -13,38 +13,38 @@ const Navbar = () => {
   return (
     <>
 
-      <header className=" body-font lg:bg-gray-100 lg:h-[274px]">
-        <div className="container mx-auto flex flex-wrap justify-between  p-5 flex-col lg:flex-row items-center">
-          <div className='flex flex-row w-[100%] lg:w-auto justify-between'>
+      <header className=" body-font lg:bg-[#F8F8F8] ">
+        <div className=" flex  justify-between   flex-col lg:flex-row items-center">
+          <div className='flex flex-row w-[100%] lg:w-auto justify-between sm:ml-[37px]  sm:mt-[37px]'>
 
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <img src={artboard} alt="" className='sm:ml-[37px]  sm:mt-[37px]  sm:w-[111px] sm:h-[111px] w-[133px] h-[133px]  mt-[39px] ' />
+            <a className="flex title-font  font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <img src={artboard} alt="" className='  sm:w-[111px] sm:h-[111px] w-[133px] h-[133px] ' />
             </a>
             <button className=' lg:hidden max-lg:pt-4  mt-[39px] ml-[10px] '> <Hamburger easing="ease-in" onToggle={clickMe} color='black' size={30} direction='left' toggled={isOpen} toggle={setIsOpen} /></button>
           </div>
-          <div className='lg:block hidden'>
-            <p className="inline-flex items-center border-0 py-1 px-3 focus:outline-none text-[#451A64] rounded text-xl font-medium  mt-4  md:mt-0">Join as Design Expert
+          <div className='mr-[94px] mt-[78px] lg:block hidden '>
+            <p className="inline-flex items-center border-0  pr-[108px]  text-[#451A64] rounded text-xl font-medium  mt-4  md:mt-0">Join as Design Expert
             </p>
-            <p className="inline-flex items-center border-0 py-1 px-3 focus:outline-none  text-[#451A64] rounded text-xl font-medium mt-4 md:mt-0">Partner with LAND Interiors
+            <p className="inline-flex items-center border-0     text-[#451A64] rounded text-xl font-medium mt-4 md:mt-0">Partner with LAND Interiors
             </p>
           </div>
         </div>
         <div>
 
-          <nav className={`md:ml-auto md:mr-auto  list-none ${isOpen? 'flex':'hidden'} animate__bounceInDown  animate__animated max-sm:flex-col    lg:flex  flex-wrap text-xl font-normal items-center text-[#000000] justify-center`}>
+          <nav className={` mt-[38px] pb-[58px]  gap-[80px] list-none ${isOpen ? 'flex' : 'hidden'} animate__bounceInDown  animate__animated max-sm:flex-col    lg:flex  flex-wrap font-normal items-center text-[#000000] justify-center`}>
 
 
-            <li className="mr-5 hover:text-gray-900">About   </li>
-            <li className="mr-5 hover:text-gray-900">Projects</li>
-            <li className="mr-5 hover:text-gray-900">Studio  </li>
-            <li className="mr-5 hover:text-gray-900">Blog    </li>
-            <li className="mr-5 hover:text-gray-900">Contact </li>
+            <li className=" text-[20px]">About   </li>
+            <li className=" text-[20px]">Projects</li>
+            <li className=" text-[20px]">Studio  </li>
+            <li className=" text-[20px]">Blog    </li>
+            <li className=" text-[20px]">Contact </li>
 
 
           </nav>
         </div>
       </header>
-  
+
 
 
 
